d9b9df7122021beb040b28395fcdfe4553b5d024
Carlos <835129+cvscarlos@users.noreply.github.com>
2024-05-24 15:38:05 +0200

    commit inicial

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+
+import { useContext } from 'react'
+import './App.css'
+import { ThemeContext } from './context/ThemeContext'
+
+function App() {
+
+  const {theme,toggleTheme} = useContext(ThemeContext)
+
+  return (
+    <>
+      <div className={`background ${theme}`}>
+        <h1 className={theme}>olá mundo!</h1>
+        <button onClick={toggleTheme} className={theme}>tema</button>
+      </div>
+
+    </>
+  )
+}
+
+export default App
