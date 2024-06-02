39e67f286f10a03549e8ca1f069eec417979167e
Carlos <835129+cvscarlos@users.noreply.github.com>
2024-06-02 16:05:08 +0200

    i18n

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,17 @@
 import { useContext } from 'react';
 import './App.css';
 import { ThemeContext } from './context/ThemeContext';
+import { useTranslation } from 'react-i18next';
 
 function App() {
 	const { theme, toggleTheme } = useContext(ThemeContext);
+	const { t } = useTranslation();
 
 	return (
 		<div className={`background ${theme}`}>
-			<h1 className={theme}>olá mundo!</h1>
+			<h1 className={theme}>{t('helloWorld')}</h1>
 			<button onClick={toggleTheme} className={theme}>
-				tema
+				{t('toggleTheme')}
 			</button>
 		</div>
 	);
