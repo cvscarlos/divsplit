a8dce90c904eacea8de092a8118bfe02e973af19
Carlos <835129+cvscarlos@users.noreply.github.com>
2024-06-02 16:08:03 +0200

    feat: add internationalization (#4)

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import { useContext } from 'react';
+import { useTranslation } from 'react-i18next';
+
 import './App.css';
 import { ThemeContext } from './context/ThemeContext';
 
 function App() {
 	const { theme, toggleTheme } = useContext(ThemeContext);
+	const { t } = useTranslation();
 
 	return (
 		<div className={`background ${theme}`}>
-			<h1 className={theme}>olá mundo!</h1>
+			<h1 className={theme}>{t('helloWorld')}</h1>
 			<button onClick={toggleTheme} className={theme}>
-				tema
+				{t('toggleTheme')}
 			</button>
 		</div>
 	);
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
+
+import './i18n.js';
 import './index.css';
+import App from './App.jsx';
 import ThemeProvider from './context/ThemeContext.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
