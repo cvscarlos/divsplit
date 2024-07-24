03804088cb3375be031de8db509ac3f5cbe5e970
Anderson Carlos <139987597+Andernial@users.noreply.github.com>
2024-07-24 17:01:35 -0300

    Anderson (#12)
    Co-authored-by: Carlos <835129+cvscarlos@users.noreply.github.com>

--- frontend/src/pages/GroupPage.jsx
+++ frontend/src/pages/GroupPage.jsx
@@ -1,11 +1,12 @@
 import { useContext, useEffect, useState } from 'react';
-
+import { useTranslation } from 'react-i18next';
 import { GroupContext } from '../context/GroupContext';
 import Header from '../components/Header';
 
 export function GroupPage() {
 	const [group, updateGroup] = useContext(GroupContext);
 	const [formFields, setFormFields] = useState({ name: 'loading...' });
+	const { t } = useTranslation();
 
 	useEffect(() => {
 		setFormFields({ name: group.header?.name || '' });
@@ -22,62 +23,75 @@ export function GroupPage() {
 	}
 
 	return (
-		<div>
+		<>
 			<Header />
-			<h2>{group.header?.name}</h2>
+			<div className="p-3 m-3 border bg-base-100 md:flex md:flex-col md:justify-center md:items-center md:border-0">
+				<h2>{group.header?.name}</h2>
 
-			<form onSubmit={handleHeaderSubmit}>
-				<div className="col-span-full">
-					<h2>Group information</h2>
-					<div className="mb-4">
-						<label className="form-control ">
-							Group name:
-							<input
-								type="text"
-								placeholder="Type here"
-								className="input input-bordered"
-								value={formFields.name}
-								name="name"
-								onChange={handleFieldChange}
-							/>
-						</label>
+				<form onSubmit={handleHeaderSubmit} className="md:border md:p-5 md:w-96">
+					<div className="">
+						<h3>{t('GroupInformation')}</h3>
+						<div className="mb-4">
+							<label className="form-control ">
+								{t('GroupName')}:
+								<input
+									type="text"
+									placeholder={t('TypeHere')}
+									className="input input-bordered"
+									value={formFields.name}
+									name="name"
+									onChange={handleFieldChange}
+								/>
+							</label>
+						</div>
 					</div>
-				</div>
 
-				<div className="col-span-full">
-					<h2>Members</h2>
-					<div className="mb-4">
-						<label className="form-control ">
-							Member name:
-							<input
-								type="text"
-								placeholder="Type here"
-								className="input input-bordered"
-								value={formFields.memberName}
-								name="memberName"
-								onChange={handleFieldChange}
-							/>
-						</label>
+					<div className="">
+						<h3>{t('Members')}</h3>
+						<div className="mb-4">
+							<label className="form-control ">
+								{t('MembersName')}:
+								<input
+									type="text"
+									placeholder={t('TypeHere')}
+									className="input input-bordered"
+									value={formFields.memberName}
+									name="memberName"
+									onChange={handleFieldChange}
+								/>
+							</label>
+							<label className="form-control ">
+								{t('PrepaidValue')}:
+								<input
+									type="number"
+									placeholder="$"
+									className="input input-bordered"
+									value={formFields.memberName}
+									name="memberName"
+									onChange={handleFieldChange}
+								/>
+							</label>
+						</div>
 					</div>
-				</div>
 
-				<div className="col-span-full">
-					<div className="mb-4">
-						<button type="submit" className="btn btn-active btn-primary">
-							Save
-						</button>
+					<div className="">
+						<div className="mb-4">
+							<button type="submit" className="btn btn-active btn-primary">
+								{t('Save')}
+							</button>
+						</div>
 					</div>
-				</div>
-			</form>
+				</form>
 
-			<div>
-				<h6>Debug:</h6>
-				<div className="mockup-code">
-					<pre>
-						<code>{JSON.stringify(group, null, 2)}</code>
-					</pre>
+				<div>
+					<h6>Debug:</h6>
+					<div className="mockup-code">
+						<pre>
+							<code>{JSON.stringify(group, null, 2)}</code>
+						</pre>
+					</div>
 				</div>
 			</div>
-		</div>
+		</>
 	);
 }
